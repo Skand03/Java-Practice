dabab51359f00939b266df809bb3b9f7e2a2cf24
Skand chaubey <skandchaubey03@gmail.com>
2025-02-27 11:50:07 +0530

    Inital commit

---
+++ variables.java
@@ -0,0 +1,27 @@
+// there are different types of variables
+// String , float , int , char and boolean
+
+public class variables {
+    public static void main(String args[]) {
+
+        // String type
+        String name = "Skand Chaubey";
+        System.out.println("My name is " + name);
+
+        // int type
+        int age = 20;
+        System.out.println("My age is " + age);
+
+        // float type
+        float height = 5.8f;
+        System.out.println("My height is " + height);
+
+        // char type
+        char myGender = 'M';
+        System.out.println("My gender is " + myGender);
+
+        // boolean type
+        boolean isStrudent = true;
+        System.out.println("Yes i am a student " + isStrudent);
+    }
+}
